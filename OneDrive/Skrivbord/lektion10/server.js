414e66099f44899128605dd25f7e14fd75c3d0dc
jakobekwall <jakob.ekwall@hotmail.com>
2022-10-10 11:31:15 +0200

    heyho lets go

---
+++ OneDrive/Skrivbord/lektion10/server.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const cors = require("cors")
+const admin = require("firebase-admin")
+const serviceAccount = require("./serviceAccount.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const app = new express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin:"*"
+}));
+
+
+app.get("/", (req, res) => {
+    res.send("yjena")
+});
+
+app.get("/shoppinglist", async (req, res) => {
+    try{
+        const shoppinglist = [];
+        const query = await db.collection("shoppinglist").get();
+
+        query.forEach(doc => {
+                shoppinglist.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+                res.json({list: shoppinglist})
+               
+        })
+        
+console.log(shoppinglist)
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+});
+
+app.put("/shoppinglist", async (req, res) => {
+    const {id} = req.body;
+
+    try {
+const ref = await db.collection("shoppinglist").doc(id).get();
+
+res.json(ref.data())
+    }
+
+catch (err) {
+    console.log(err)
+}
+})
+
+
+app.listen(1234, () => {
+    console.log("eyy lissna din lille nacas")
+})
